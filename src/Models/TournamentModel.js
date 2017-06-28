da14e8e0ba62d280a499669338df17e280579e45
matrenin@ukr.net <matrenin@ukr.net>
2017-06-28 13:21:44 +0300

    Join improvements

--- src/Models/TournamentModel.js
+++ src/Models/TournamentModel.js
@@ -12,6 +12,30 @@ module.exports = class TournamentModel {
     return `tournaments:${this._tournamentId}:deposit`;
   }
 
+  _keyPlayer(playerId) {
+    return `tournaments:${this._tournamentId}:player:${playerId}`;
+  }
+
+  _keyBacker(playerId,backerId) {
+    return this._keyPlayer(playerId)+`:backer:${backerId}`;
+  }
+
+  checkExistPlayer(playerId) {
+    return new Promise((resolve,reject) => {
+      redisClient.get(this._keyPlayer(playerId),(err, reply) => {
+        if(err) {
+          reject(err);
+        } else {
+          if(reply && reply === 'ok') {
+            reject('Player already in the tournament.');
+          } else {
+            resolve();
+          }
+        }
+      });
+    });
+  }
+
   checkExist() {
     return new Promise((resolve,reject) => {
       redisClient.get(this._keyDeposit(),(err, reply) => {
@@ -44,14 +68,34 @@ module.exports = class TournamentModel {
         if(!exist.isExist) {
           reject('Tournament does not exist.');
         } else {
+
+          // Check all players can join to the tournament.
           const players = backers.concat([playerId]);
           const selfDeposit = Math.ceil(exist.deposit / players.length);
           const promises = players.map(player => {
             const playerModel = new PlayerModel(player);
             return playerModel.pointsCan(selfDeposit);
           });
+
+          // Check player does not exist on the tournament.
+          promises.push(this.checkExistPlayer(playerId));
+
+          // Funds and add players to the store.
           Promise.all(promises).then(() => {
-            resolve();
+            const promisesFund = [];
+
+            redisClient.set(this._keyPlayer(playerId), 'ok');
+            const playerModel = new PlayerModel(playerId);
+            promisesFund.push(playerModel.take(selfDeposit));
+
+            backers.forEach((backerId) => {
+              redisClient.set(this._keyBacker(playerId,backerId), 'ok');
+
+              const backerModel = new PlayerModel(backerId);
+              promisesFund.push(backerModel.take(selfDeposit));
+            });
+
+            Promise.all(promisesFund).then(resolve).catch(reject);
           }).catch(reject);
         }
       });
